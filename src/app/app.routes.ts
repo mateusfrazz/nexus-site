0f2ea78e73ed4f5d09181d1b339a56f48494454a
mateus <mateusfrazao12345@gmail.com>
2026-02-05 11:35:34 -0300

    enviando as alterações locais

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,15 @@
 import { Routes } from '@angular/router';
 import path from 'path';
+import { Home } from './pages/home/home';
 
 
 export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    },
+    {
+        path: 'home',
+        component: Home
+    }
 ];
--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatIconModule, CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
